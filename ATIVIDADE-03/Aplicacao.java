3a7ed16529540b9fc2c1f540e662402fa6232689
juliotrigueiroo <159463745+juliotrigueiroo@users.noreply.github.com>
2024-08-27 12:34:20 -0300

    Add files via upload

---
+++ ATIVIDADE-03/Aplicacao.java
@@ -0,0 +1,46 @@
+package prjPokémon;
+
+public class Aplicacao {
+	public static void main(String[] args) {
+		Pokemon charmander = new Pokemon();
+		charmander.atributoNome = "charmander";
+		charmander.atributoTipo = "fogo";
+		charmander.atributoVida = 15;
+		charmander.atributoNivel = 1; 
+		
+		Pokemon charizarde = new Pokemon();
+		charizarde.atributoNome = "charizarde";
+		charizarde.atributoTipo = "fogo e voador";
+		charizarde.atributoVida = 1000;
+		charizarde.atributoNivel = 3;
+		
+		Pokemon pikachu = new Pokemon();
+		pikachu.atributoNome = "pikachu";
+		pikachu.atributoTipo = "eletrico";
+		pikachu.atributoVida = 15;
+		pikachu.atributoNivel = 1;
+		
+		Pokemon frogadier = new Pokemon();
+		frogadier .atributoNome = "frogadier ";
+		frogadier .atributoTipo = "água";
+		frogadier .atributoVida = 60;
+		frogadier .atributoNivel = 2;
+		
+		Pokemon pichu = new Pokemon();
+		pichu.atributoNome = "pichu  ";
+		pichu.atributoTipo = "eletrico";
+		pichu.atributoVida = 22;
+		pichu.atributoNivel = 1;
+		
+		Pokemon raiochu = new Pokemon();
+		raiochu.atributoNome = "raiochu  ";
+		raiochu.atributoTipo = "eletrico";
+		raiochu.atributoVida = 220;
+		raiochu.atributoNivel = 3;
+		
+		
+		
+		
+	}
+
+}
---
+++ ATIVIDADE-03/pokemon.java
@@ -0,0 +1,27 @@
+package prjPokémon;
+
+public class Pokemon {
+	//Atributos
+		String atributoNome;
+		String atributoTipo;
+		int atributoVida;
+		int atributoNivel;
+		
+		//Construtores
+		public Pokemon() {
+			
+		}
+		public Pokemon(String parametroNome, String parametroTipo, int parametroVida, int parametroNivel) {
+			this.atributoNome = parametroNome;
+			this.atributoTipo = parametroTipo;
+			this.atributoVida = parametroVida;
+			this.atributoNivel = parametroNivel;
+		}
+		//metodos
+		
+		public void modoAtaque() {
+			
+		}
+		
+
+}
